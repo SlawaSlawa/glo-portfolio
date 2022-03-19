82eee6ebd7de75076523a926d07616eb6cc3d903
SlawaSlawaSlawa <100555254+SlawaSlawaSlawa@users.noreply.github.com>
2022-03-19 09:58:51 -0700

    added function: tabs

---
+++ js/tabs.js
@@ -0,0 +1,34 @@
+const tabs = () => {
+    const tabBtns = document.querySelectorAll('.design-list__item')
+    const tabContents = document.querySelectorAll('.design__descr')
+    const tabImages = document.querySelectorAll('.design-images')
+
+    const changeContent = (content, data) => {
+        console.log(content, data);
+        content.forEach(elem => {
+            if (data === elem.dataset.tabsField) {
+                elem.classList.remove('hidden')
+            } else {
+                elem.classList.add('hidden')
+            }
+        })
+    }
+
+    tabBtns.forEach(btn => {
+        btn.addEventListener('click', (evt) => {
+            const target = evt.target
+
+            tabBtns.forEach(tabBtn => {
+                if (tabBtn === btn) {
+                    tabBtn.classList.add('design-list__item_active')
+                } else {
+                    tabBtn.classList.remove('design-list__item_active')
+                }
+            })
+            changeContent(tabContents, target.dataset.tabsHandler)
+            changeContent(tabImages, target.dataset.tabsHandler)
+        })
+    })
+}
+
+tabs()
